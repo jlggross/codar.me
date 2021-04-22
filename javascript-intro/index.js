d6fb2a87c090e28b29087b8209b2e30127191899
jlggross <joaolggross@gmail.com>
2021-04-22 18:12:30 -0300

    Javascript intro course
    These are all the remaining classes for the javaScripot introduction course

---
+++ javascript-intro/index.js
@@ -0,0 +1,7 @@
+// Import for 'export default'
+//import sub from "./sub.js"
+
+// Import for named function
+import { sub } from "./sub.js"
+
+console.log(sub(1, 4))
---
+++ javascript-intro/sub.js
@@ -0,0 +1,7 @@
+const sub = (n1, n2) => n1 - n2
+
+// Single value export
+//export default sub
+
+// Name export
+export { sub }
