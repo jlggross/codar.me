83b5952efb8fdf974497cf7cf0a74736c27bf8a6
jlggross <joaolggross@gmail.com>
2021-04-26 20:16:11 -0300

    First Restify Server

---
+++ nodejs-intro/class18-middlewares/restify/index.js
@@ -0,0 +1,11 @@
+const restify = require('restify')
+
+const server = restify.createServer()
+
+server.get('/', (req, res) => {
+  res.send('Hello World.')
+})
+
+server.listen(8080, () => console.log('Server up.'))
+
+// GET http://localhost:8080
