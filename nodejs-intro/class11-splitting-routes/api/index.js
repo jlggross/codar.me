bff9c837f7d99a355a368e3e0567000d0ea0a789
jlggross <joaolggross@gmail.com>
2021-04-25 21:04:36 -0300

    Adding node_modules to .gitignore

---
+++ nodejs-intro/class11-splitting-routes/api/index.js
@@ -0,0 +1,52 @@
+const http = require('http')
+const mongoose = require('mongoose')
+
+const aa = 'test'
+const hello = (test) => {
+  console.log('Hello')
+}
+
+// mongodb://user:password@host:port/database-name
+// localhost could also be 127.0.0.0 or 0.0.0.0
+// authSource tag so mongoose know where to make the authentication
+mongoose.connect(
+  'mongodb://root:example@localhost:27017/api?authSource=admin',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+)
+
+mongoose.connection.on('error', () => console.log('MongoDB: Error!'))
+mongoose.connection.on('open', () => console.log('MongoDB: Connected!'))
+
+const schema = new mongoose.Schema(
+  {
+    name: String,
+  },
+  {
+    timestamps: true,
+  }
+)
+const UserModel = mongoose.model('User', schema)
+// Mongoose will parse the string and create a table 'users'
+
+const server = http.createServer(async (req, res) => {
+  // Insert data into the database
+  // We run this just once to populate the database
+  // await UserModel.create({
+  //  name: "João Gross"
+  // })
+
+  // Get data from the database
+  const users = await UserModel.find({})
+  console.log(users)
+
+  // Send a response to our use (browser)
+  res.writeHead(200, { 'Context-Type': 'application/json' })
+  res.write(JSON.stringify(users))
+  res.end()
+})
+
+// Starts the server at localhost:8080
+server.listen(8080)
